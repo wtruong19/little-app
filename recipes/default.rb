67c596f7edf3fc7bf54366be9b753c8396a64f70
wtruong19 <william.c.truong@gmail.com>
2015-05-29 02:46:16 +0000

    create hello world index and edit default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+service 'iptables' do
+  action :stop
+end
